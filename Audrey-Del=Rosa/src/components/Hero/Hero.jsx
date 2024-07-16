9ee97632141d2a195927bcf31d9bc7f2be30c187
Audrey <delrosaa@tcd.ie>
2024-07-16 22:07:10 +0100

    intro section

--- Audrey-Del=Rosa/src/components/Hero/Hero.jsx
+++ Audrey-Del=Rosa/src/components/Hero/Hero.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { getImageUrl } from '../../utils';
 import styles from './Hero.module.css';
+import Image from 'react-bootstrap/Image';
+import { MdMailOutline } from "react-icons/md";
+
 const Hero = () => {
     return (
         <section className={styles.container} >
             <div className={styles.content} >
-                <h1  className={styles.title}>Hi, I'm Audrey</h1>
-                <p className={styles.description}>I am a Software Engineer. 
-                    With experience in React. Reach out to learn more
-
-                </p>
+                <Image src={getImageUrl("hero/me.png")}
+                    roundedCircle
+                    alt="Hero Image of me" 
+                    className={styles.heroImg}
+                />
+                <h1  className={styles.title}>hi, i'm audrey</h1>
+                    <p className={styles.description}>I'm a 3rd year Computer Science student based in Dublin, Ireland. 
+                        I have a wide range of interests from Full Stack Development, AI, Finance, and UI Design! </p>
                 <a href="mailto:delrosaa@tcd.ie"
-                className={styles.contactBtn}>Contact Me</a>
+                target="_blank" rel="noopener noreferrer"
+                className={styles.contactBtn}>
+                    <MdMailOutline/>
+                    <p className={styles.btnText}>reach out :)</p>
+                </a>
             </div>
-            <img src={getImageUrl("hero/heroImage.png")}
-             alt="Hero Image of me" 
-             className={styles.heroImg}/>
+            
             <div className={styles.topBlur}/>
             <div className={styles.bottomBlur}/>
         </section>
