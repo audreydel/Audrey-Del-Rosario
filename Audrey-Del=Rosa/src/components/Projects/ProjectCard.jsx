51605e8b4c5bc3ee38681e95953f2252c9748c64
Audrey <delrosaa@tcd.ie>
2024-07-16 22:07:41 +0100

    adding project card transitions

--- Audrey-Del=Rosa/src/components/Projects/ProjectCard.jsx
+++ Audrey-Del=Rosa/src/components/Projects/ProjectCard.jsx
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getImageUrl } from '../../utils';
 import  styles from './ProjectCard.module.css';
 
 const ProjectCard = ({ project : {title, imageSrc, description, skills, demo, source}}) => {
+
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const flipCard = () => {
+        setIsFlipped(!isFlipped);
+    };
+
     return (
         <div className={styles.container}>
-            <img 
-                src={getImageUrl(imageSrc)} 
-                alt={`${title} Title`} 
-                className={styles.image}
-            />
-            <h3 className={styles.title}>{title}</h3>
-            <p className={styles.description}>{description}</p>
-            <ul className={styles.skills}>
-                {
-                    skills.map((skill, id) => {
-                        return ( 
-                            <li key={id} className={styles.skill}>{skill}</li>
-                        );
-                    })
-                    
-                }
-            </ul>
-            <div className={styles.links}>
-                <a href={demo} className={styles.link}>Demo</a>
-                <a href={source} className={styles.link}>Source</a>
+            <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} onClick={flipCard}>
+                <div className={styles.front}>
+                    <div className={styles.top}>
+                        <img
+                            src={getImageUrl(imageSrc)}
+                            alt={`${title} Title`}
+                            className={styles.image}
+                        />
+                        <h3 className={styles.title}>{title}</h3>
+                    </div>
+                    <ul className={styles.skills}>
+                        {skills.map((skill, id) => (
+                            <li key={id} className={styles.skill}>
+                                {skill}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.back}>
+                    <p className={styles.description}>{description}</p>
+                    <div className={styles.links}>
+                        <a href={demo} className={styles.link} target="_blank" rel="noopener noreferrer">
+                            Demo
+                        </a>
+                        <a href={source} className={styles.link} target="_blank" rel="noopener noreferrer">
+                            Source
+                        </a>
+                    </div>
+                </div>
             </div>
-                        
         </div>
+
     );
 };
 
