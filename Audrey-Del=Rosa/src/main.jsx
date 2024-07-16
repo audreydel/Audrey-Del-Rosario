fe3ffb151ac7b7b6df59fe0493d07a56b61c9299
Audrey <delrosaa@tcd.ie>
2024-07-16 22:08:33 +0100

    misc changes

--- Audrey-Del=Rosa/src/main.jsx
+++ Audrey-Del=Rosa/src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import '@fontsource/outfit';
-import '@fontsource/roboto';
+// import '@fontsource/outfit';
+// import '@fontsource/roboto';
+import '@fontsource/prosto-one';
+import '@fontsource/fahkwang';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
