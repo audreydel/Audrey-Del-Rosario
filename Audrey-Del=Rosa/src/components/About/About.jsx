9a1fb7e68b5018ac2fd1ede29b3806d20bc14d32
Audrey <delrosaa@tcd.ie>
2024-07-18 19:17:09 +0100

    changes

--- Audrey-Del=Rosa/src/components/About/About.jsx
+++ Audrey-Del=Rosa/src/components/About/About.jsx
@@ -1,46 +1,76 @@
 import { getImageUrl } from '../../utils';
 import styles from './About.module.css';
-
-import React from 'react';
+import { FaCodeBranch } from "react-icons/fa6";
+import { GiSewingMachine } from "react-icons/gi";
+import { FaLaptopCode } from "react-icons/fa";
 
 const About = () => {
     return (
         <section className={styles.container} id="about" >
-            <h2 className={styles.title}>About Me!</h2>
+            <h2 className={styles.title}> ⋆ a little bit about me ⋆</h2>
             <div className={styles.content}>
                 <ul className={styles.aboutItems}>
                     <li className={styles.aboutItem}>
-                        <img src={getImageUrl('about/cursorIcon.png')} 
-                                alt="cursorIcon" 
-                        />
+                        <div>
+                            <FaLaptopCode className={styles.aboutItemIconLaptop}/>
+                        </div>
+                        
                         <div  className={styles.aboutItemText}>
-                            <h3>Frontend Developer   </h3>
-                            <p>I'm a frontend develoer with expeirnce buiilding
-                                websites that slay 
-                            </p>
+                            <h3>Work & College </h3>
+                            <ul>
+                                <li>
+                                I am currently a Software Engineering Intern at Dell Technologies. 
+                                </li>
+                                <li>
+                                I am an incoming 3rd year Computer Science & Business 
+                                student at Trinity College Dublin. 
+                                </li>
+                                <li>
+                                Some languages I've worked with are: 
+                                    <li>
+                                    Java | Python | C++ | JavaScript | TypeScript
+                                    </li>
+                                </li>
+                            </ul>
+                            
+                                
                         </div>
                     </li>
-
                     <li  className={styles.aboutItem}>
-                        <img src={getImageUrl('about/serverIcon.png')} 
-                                alt="cursorIcon" 
-                        />
+                        <div>
+                            <FaCodeBranch className={styles.aboutItemIconNetwork}/>
+                        </div>
                         <div className={styles.aboutItemText}>
-                            <h3>Backend Developer   </h3>
-                            <p>I have experience developing fast & optimised backend 
-                                systems & APIs
-                            </p>
+                            <h3>Full Stack Development </h3>
+                            <ul>
+                                <li>
+                                    I enjoy building end-to-end Full Stack applications in industry 
+                                    settings & in my personal time. 
+                                </li>
+                                <li>
+                                Starting off with an idea to solve a problem 
+                                    to designing the UI to coding up a solution.
+                                </li>
+                            </ul>
+                                
+                                
+                            
                         </div>
                     </li>
+                    
+
+                    
 
                     <li  className={styles.aboutItem}>
-                        <img src={getImageUrl('about/uiIcon.png')} 
-                                alt="uil" 
-                        />
+                        <div>
+                            <GiSewingMachine className={styles.aboutItemIconSew}/>
+                        </div>
                         <div className={styles.aboutItemText}>
-                            <h3>UI Designer  </h3>
-                            <p>I have designed multiple landing pages & having experience creating design systems as well
-                            </p>
+                            <h3>Other Hobbies   </h3>
+                            <ul>
+                                <li>Outside of work and academics, I enjoy sewing clothes, drawing, and reading.
+                                </li>
+                            </ul>
                         </div>
                     </li>
 
