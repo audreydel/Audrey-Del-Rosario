39ade9dc1414fdd34320e874dcdcd9f720cf6c54
Audrey <delrosaa@tcd.ie>
2024-07-18 18:18:43 +0100

    layout change

--- Audrey-Del=Rosa/src/components/Hero/Hero.jsx
+++ Audrey-Del=Rosa/src/components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                     className={styles.heroImg}
                 />
                 <h1  className={styles.title}>hi, i'm audrey</h1>
-                    <p className={styles.description}>I'm a 3rd year Computer Science student based in Dublin, Ireland. 
+                    <p className={styles.description}>I'm a 3rd year Computer Science & Business student based in Dublin, Ireland. 
                         I have a wide range of interests from Full Stack Development, AI, Finance, and UI Design! </p>
                 <a href="mailto:delrosaa@tcd.ie"
                 target="_blank" rel="noopener noreferrer"
