4b93200bcb82391ebd8193200fca705fea6c5705
Audrey <delrosaa@tcd.ie>
2024-07-19 08:16:53 +0100

    adding cv

--- Audrey-Del=Rosa/src/components/Navbar/Navbar.jsx
+++ Audrey-Del=Rosa/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import styles from './Navbar.module.css'
-import { getImageUrl } from '../../utils';
 import { MdEmail } from "react-icons/md";
 import { FaGithubSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { IoMenu, IoCloseOutline } from "react-icons/io5";
-
+import { Tooltip } from 'react-tooltip'
+import { IoIosPaper } from "react-icons/io";
+import { TbMailOpenedFilled } from "react-icons/tb";
 
 
 const Navbar = () => {
@@ -37,19 +38,43 @@ const Navbar = () => {
                 </div>
                 <a href="mailto:delrosaa@tcd.ie"
                     target="_blank" rel="noopener noreferrer"
+                    data-tooltip-id="mail-tip"
+                    data-tooltip-content="my email"
+                    data-tooltip-place="bottom"
                 >
-                    <MdEmail className={styles.link}/>
+                    <TbMailOpenedFilled className={styles.link}/>
                 </a>
+
+                <Tooltip id="mail-tip"/>
                 <a href="https://www.linkedin.com/in/audreydelrosa"
                     target="_blank" rel="noopener noreferrer"
+                    data-tooltip-id="linkedin-tip"
+                    data-tooltip-content="my linkedin"
+                    data-tooltip-place="bottom"
                 >
                     <FaLinkedin className={styles.link}/>
                 </a>
+                <Tooltip id="linkedin-tip"/>
+
                 <a href="https://github.com/audreydel"
                     target="_blank" rel="noopener noreferrer"
+                    data-tooltip-id="github-tip"
+                    data-tooltip-content="my github"
+                    data-tooltip-place="bottom"
                 >
                     <FaGithubSquare className={styles.link}/>
                 </a>
+                <Tooltip id="github-tip"/>
+
+                <a href="../../assets/cv/ADR-CV.pdf"
+                    target="_blank" rel="noopener noreferrer"
+                    data-tooltip-id="cv-tip"
+                    data-tooltip-content="my cv"
+                    data-tooltip-place="bottom"
+                >
+                    <IoIosPaper className={styles.link}/>
+                </a>
+                <Tooltip id="cv-tip"/>
                 
             </div>
             
